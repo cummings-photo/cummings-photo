0b2906a7e49050332c0cf5eb9eb0701a77f44b3c
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-01-18 23:43:11 -0500

    Update lightpics.js

--- html-code/js/lightpics.js
+++ html-code/js/lightpics.js
@@ -1,4 +1,4 @@
 const images = [
-   {"url": "../ART/RORA INTEL/lghtwrks.png", "label": "product description"},
-   {"url": "../ART/RORA INTEL/LIGHTWORKS2.png", "label": "schematic, ink on 11 x 17 in. graphpaper, september 2019"},
+   {"url": "../ART/RORA%20INTEL/lghtwrks.png", "label": "product description"},
+   {"url": "../ART/RORA%20INTEL/LIGHTWORKS2.png", "label": "schematic, ink on 11 x 17 in. graphpaper, september 2019"},
 ]
