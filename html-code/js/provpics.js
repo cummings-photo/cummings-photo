07d13911801338fd93727eb564093c4e90f57c50
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-01-18 23:44:18 -0500

    Update provpics.js

--- html-code/js/provpics.js
+++ html-code/js/provpics.js
@@ -1,4 +1,4 @@
 const images = [
-   {"url": "../ART/RORA INTEL/specs.png", "label": "product description"},
-   {"url": "../ART/RORA INTEL/provacateur.png", "label": "schematic, mixed media on 11 x 17 in. graphpaper, october 2019"},
+   {"url": "../ART/RORA%20INTEL/specs.png", "label": "product description"},
+   {"url": "../ART/RORA%20INTEL/provacateur.png", "label": "schematic, mixed media on 11 x 17 in. graphpaper, october 2019"},
 ]
