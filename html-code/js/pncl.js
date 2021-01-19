76ce016f79827b6947215e2cf5645f00298f5550
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-01-18 23:43:46 -0500

    Update pncl.js

--- html-code/js/pncl.js
+++ html-code/js/pncl.js
@@ -1,4 +1,4 @@
 const images = [
-   {"url": "../ART/RORA INTEL/pns.png", "label": "product description"},
-   {"url": "../ART/RORA INTEL/pencilnyamma.png", "label": "schematic, ink on 11 x 17 in. graphpaper, november 2019"},
+   {"url": "../ART/RORA%20INTEL/pns.png", "label": "product description"},
+   {"url": "../ART/RORA%20INTEL/pencilnyamma.png", "label": "schematic, ink on 11 x 17 in. graphpaper, november 2019"},
 ]
