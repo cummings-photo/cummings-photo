a103d95344455ede7e2a8540e45f77b4ef19f884
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-01-18 23:42:35 -0500

    Update goyapics.js

--- html-code/js/goyapics.js
+++ html-code/js/goyapics.js
@@ -1,5 +1,5 @@
 const images = [
-   {"url": "../ART/RORA INTEL/goyatit.png", "label": "product title"},
-   {"url": "../ART/RORA INTEL/goyades.png", "label": "mission statement"},
-   {"url": "../ART/RORA INTEL/goya.png", "label": "schematic, ink on 18 x 24 in. watercolor paper, november 2020"},
+   {"url": "../ART/RORA%20INTEL/goyatit.png", "label": "product title"},
+   {"url": "../ART/RORA%20INTEL/goyades.png", "label": "mission statement"},
+   {"url": "../ART/RORA%20INTEL/goya.png", "label": "schematic, ink on 18 x 24 in. watercolor paper, november 2020"},
 ]
