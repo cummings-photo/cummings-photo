428038486ed3962c1bceefd429561f41296089f3
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-01-22 01:22:41 -0500

    Update goyapics.js
    fixed missing mission statement

--- html-code/js/goyapics.js
+++ html-code/js/goyapics.js
@@ -1,5 +1,5 @@
 const images = [
    {"url": "../ART/RORA%20INTEL/goyatit.png", "label": "product title"},
-   {"url": "../ART/RORA%20INTEL/goyades.png", "label": "mission statement"},
+   {"url": "../ART/RORA%20INTEL/goyawriter.png", "label": "mission statement"},
    {"url": "../ART/RORA%20INTEL/goya.png", "label": "schematic, ink on 18 x 24 in. watercolor paper, november 2020"},
 ]
