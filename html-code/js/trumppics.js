04691bdd291ed7e39bc3d7f827c8bac6905c95e6
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-01-27 02:07:51 -0500

    Update trumppics.js

--- html-code/js/trumppics.js
+++ html-code/js/trumppics.js
@@ -1,4 +1,4 @@
 const images = [
    {"url": "../ART/RORA%20INTEL/trumpwriter.png", "label": "mission statement"},
-   {"url": "../ART/RORA%20INTEL/trumpito.png", "label": "Trump's Visit, mixed media on 18 x 24 in. watercolor paper, january 2020"},
+   {"url": "../ART/RORA%20INTEL/trumpito.png", "label": "Trump's Visit, graphite on 18 x 24 in. watercolor paper, january 2020"},
 ]
