0011739ef953a31154904127074472b2242e62e4
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-01-26 20:49:22 -0500

    Update trumppics.js

--- html-code/js/trumppics.js
+++ html-code/js/trumppics.js
@@ -1,3 +1,4 @@
 const images = [
-    {"url": "file:///Users/christiancummings/Desktop/ART/RORA%20INTEL/underconstruct.jpeg", "label":"Please bare with us. This project is still undergoing testing. It will  be unveiled by our deadline date on January 20, 2021 (Inauguration Day)."},
+   {"url": "../ART/RORA%20INTEL/trumpwriter.png", "label": "mission statement"},
+   {"url": "../ART/RORA%20INTEL/trumpito.png", "label": "Trump's Visit, mixed media on 11 x 17 in. graphpaper, january 2020"},
 ]
