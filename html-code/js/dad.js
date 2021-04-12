0e1e031e7398a8a002e6f7356c40b4e9d410369d
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-04-11 20:46:20 -0400

    Update dad.js

--- html-code/js/dad.js
+++ html-code/js/dad.js
@@ -1,7 +1,7 @@
 const images = [
 {"url": "../ART/dad/dadbest.png", "label": "sharon by robert cummings, 2018"},
-{"url": "../ART/dad/IMG_1919.png", "label": "runningback by robert cummings"},
-{"url": "../ART/dad/IMG_1962.png", "label": "guess who by robert cummings"},
-{"url": "../ART/dad/IMG_1934.png", "label": "pure shooter by robert cummings"},
-{"url": "../ART/dad/IMG_1959.png", "label": "church lady by robert cummings"},
+{"url": "../ART/dad/IMG_1919.jpeg", "label": "runningback by robert cummings"},
+{"url": "../ART/dad/IMG_1962.jpeg", "label": "guess who by robert cummings"},
+{"url": "../ART/dad/IMG_1934.jpeg", "label": "pure shooter by robert cummings"},
+{"url": "../ART/dad/IMG_1959.jpeg", "label": "church lady by robert cummings"},
 ]
