de970b37b6cead56d763c73d4afc9656399f02e6
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-05-23 23:11:06 -0400

    Add files via upload

---
+++ html-code/js/blckcrtns.js
@@ -0,0 +1,7 @@
+const images = [
+   {"url" : "../ART/blackcartoons/IMG_2004.jpeg", "label": "’85 SL500, 2021"},
+   {"url" : "../ART/blackcartoons/chase.png", "label": "(paul) mooney toons, 2021"},
+   {"url" : "../ART/blackcartoons/IMG_1999.jpeg", "label": "’70 M3, 2021"},
+   {"url" : "../ART/blackcartoons/traininghaze.png", "label": "training day, 2021"},
+
+]
