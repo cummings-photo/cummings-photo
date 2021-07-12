77e638eb2e27cf43df9d92d610c62eda3cab195b
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-07-11 21:19:48 -0400

    Update tielist.js

--- html-code/js/tielist.js
+++ html-code/js/tielist.js
@@ -1,7 +1,7 @@
 const images = [
    {"url" : "../ART/entangled/sadlove.jpeg", "label": "imperfect union, 2021"},
-   {"url" : "../ART/entanlged/confusion.jpeg", "label": "confusion, 2021"},
-   {"url" : "../ART/entanlged/entanglements.jpeg", "label": "entangled, 2021"},
-   {"url" : "../ART/entanlged/bruk.jpeg", "label": "bruk, 2021"},
+   {"url" : "../ART/entangled/confusion.jpeg", "label": "confusion, 2021"},
+   {"url" : "../ART/entangled/entanglements.jpeg", "label": "entangled, 2021"},
+   {"url" : "../ART/entangled/bruk.jpeg", "label": "bruk, 2021"},
 ]
    
