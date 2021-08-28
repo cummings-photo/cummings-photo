12ffd780a2c02d83541429314a49a10e94731695
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-08-28 00:08:31 -0400

    Update writnav.js

--- html-code/js/writnav.js
+++ html-code/js/writnav.js
@@ -1,10 +1,10 @@
 window.onkeyup = function (e) {
   let key = e.keyCode ? e.keyCode : e.which;
   if (key === 38) {
-   try { window.location.replace("https://studio-cjc.com/html-code/collage.html"); } 
-   catch(e) { window.location = "https://studio-cjc.com/html-code/collage.html"; };
+   try { window.location.replace("https://studio-cjc.com/html-code/campaigns.html"); } 
+   catch(e) { window.location = "https://studio-cjc.com/html-code/campaigns.html"; };
   } else if (key === 40) {
-   try { window.location.replace("https://studio-cjc.com/html-code/writing.html"); } 
-   catch(e) { window.location = "https://studio-cjc.com/html-code/writing.html"; };
+   try { window.location.replace("https://studio-cjc.com/html-code/roraintel.html"); } 
+   catch(e) { window.location = "https://studio-cjc.com/html-code/roraintel.html"; };
   } 
 }
