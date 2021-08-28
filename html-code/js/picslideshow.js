fec81f59cf99edd17c3f8f7b4eefb38eafc4e488
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-08-28 00:56:18 -0400

    Update picslideshow.js

--- html-code/js/picslideshow.js
+++ html-code/js/picslideshow.js
@@ -43,6 +43,14 @@ window.onkeyup = function (e) {
     prev();
   } else if (key === 39) {
     next();
+  }
+  
+    else if (key === 38) {
+    try { window.location.replace("https://studio-cjc.com/html-code/index.html"); } 
+    catch(e) { window.location = "https://studio-cjc.com/html-code/index.html"; };
+  } else if (key === 40) {
+   try { window.location.replace("https://studio-cjc.com/html-code/collage.html"); } 
+   catch(e) { window.location = "https://studio-cjc.com/html-code/collage.html"; };
   } 
 }
 
