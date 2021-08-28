df37c40e08291a560e0dd581b6a1a72d045a4b98
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-08-27 23:51:08 -0400

    Update campnav.js

--- html-code/js/campnav.js
+++ html-code/js/campnav.js
@@ -1,10 +1,10 @@
 window.onkeyup = function (e) {
   let key = e.keyCode ? e.keyCode : e.which;
   if (key === 38) {
-   try { window.location.replace("https://studio-cjc.com/html-code/photos.html"); } 
-   catch(e) { window.location = "https://studio-cjc.com/html-code/photos.html"; };
+   try { window.location.replace("https://studio-cjc.com/html-code/collage.html"); } 
+   catch(e) { window.location = "https://studio-cjc.com/html-code/collage.html"; };
   } else if (key === 40) {
-   try { window.location.replace("https://studio-cjc.com/html-code/campaigns.html"); } 
-   catch(e) { window.location = "https://studio-cjc.com/html-code/campaigns.html"; };
+   try { window.location.replace("https://studio-cjc.com/html-code/writing.html"); } 
+   catch(e) { window.location = "https://studio-cjc.com/html-code/writing.html"; };
   } 
 }
