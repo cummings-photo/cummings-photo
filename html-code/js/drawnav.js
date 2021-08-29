ea5e025940edbb6a2a8ac4c3bab4efeedc4c018d
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-08-29 17:50:51 -0400

    Update drawnav.js

--- html-code/js/drawnav.js
+++ html-code/js/drawnav.js
@@ -6,5 +6,8 @@ window.onkeyup = function (e) {
   } else if (key === 40) {
    try { window.location.replace("https://studio-cjc.com/html-code/index.html"); } 
    catch(e) { window.location = "https://studio-cjc.com/html-code/index.html"; };
+  } else if (key === 39) {
+   try { window.location.replace("https://studio-cjc.com/html-code/MJ.html"); } 
+   catch(e) { window.location = "https://studio-cjc.com/html-code/MJ.html"; };
   } 
 }
