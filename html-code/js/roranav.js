57ccb13d3a962e15fa536acd790ad4b03f653fa1
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-08-29 17:49:48 -0400

    Update roranav.js

--- html-code/js/roranav.js
+++ html-code/js/roranav.js
@@ -6,5 +6,9 @@ window.onkeyup = function (e) {
   } else if (key === 40) {
    try { window.location.replace("https://studio-cjc.com/html-code/drawings.html"); } 
    catch(e) { window.location = "https://studio-cjc.com/html-code/drawings.html"; };
+  } else if (key === 39) {
+   try { window.location.replace("https://studio-cjc.com/html-code/lghtwrks.html"); } 
+   catch(e) { window.location = "https://studio-cjc.com/html-code/lghtwrks.html"; };
   } 
+  
 }
