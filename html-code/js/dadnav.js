998c752bc1ce0c90604aa4a5ba1ab7f39cad4f0e
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-08-29 20:04:57 -0400

    Create dadnav.js

---
+++ html-code/js/dadnav.js
@@ -0,0 +1,59 @@
+const slideshow = document.querySelector(".slideshow");
+for (let i = 0; i < images.length; i++) {
+  slideshow.innerHTML += ('<div class="slide" style="background-image: url(' + images[i].url + ')">' + '<div class="slide-label">' + images[i].label + '</div></div>');
+}
+
+const slides = document.querySelectorAll('.slide');
+const slideCount = slides.length;
+let idx = 0;
+
+
+
+function showImg () {
+  slides[idx].setAttribute('class', 'slide-active')
+}
+
+function hideImg () {
+  slides[idx].setAttribute('class', 'slide')
+}
+
+function openLightbox() {
+  document.getElementById('Lightbox').style.display = 'block'; 
+};
+    
+function closeLightbox() {
+  document.getElementById('Lightbox').style.display = 'none';
+};
+
+function prev () {
+  hideImg();
+  idx = (idx - 1 + slideCount) % slideCount;
+  showImg();
+}
+
+function next () {
+  hideImg();
+  idx = (idx + 1) % slideCount;
+  showImg();
+}
+
+window.onkeyup = function (e) {
+  let key = e.keyCode ? e.keyCode : e.which;
+  if (key === 37) {
+    prev();
+  } else if (key === 39) {
+    next();
+  }
+  
+    else if (key === 38) {
+    try { window.location.replace("https://studio-cjc.com/html-code/brochant.html"); } 
+    catch(e) { window.location = "https://studio-cjc.com/html-code/brochant.html"; };
+  } else if (key === 40) {
+   try { window.location.replace("https://studio-cjc.com/html-code/blckcrtns.html"); } 
+   catch(e) { window.location = "https://studio-cjc.com/html-code/blckcrtns.html"; };
+  } 
+}
+
+
+
+showImg();
