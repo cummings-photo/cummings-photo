8cc0fdd3b4836853bd88445892e338f6f00055b1
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2021-08-29 20:52:16 -0400

    Create joynav.js

---
+++ html-code/js/joynav.js
@@ -0,0 +1,16 @@
+window.onkeyup = function (e) {
+  let key = e.keyCode ? e.keyCode : e.which;
+  //if (key === 38) {
+   //try { window.location.replace("https://studio-cjc.com/html-code/collage.html"); } 
+   //catch(e) { window.location = "https://studio-cjc.com/html-code/collage.html"; };
+  }//else if (key === 40) {
+   //try { window.location.replace("https://studio-cjc.com/html-code/writing.html"); } 
+   //catch(e) { window.location = "https://studio-cjc.com/html-code/writing.html"; };
+  } //else if (key === 39) {
+   //try { window.location.replace("https://studio-cjc.com/html-code/joydes1.html"); } 
+   c//atch(e) { window.location = "https://studio-cjc.com/html-code/joydes1.html"; };
+  } else if (key === 37) {
+   try { window.location.replace("https://studio-cjc.com/html-code/campaigns.html"); } 
+   catch(e) { window.location = "https://studio-cjc.com/html-code/campaigns.html"; };
+  } 
+}
