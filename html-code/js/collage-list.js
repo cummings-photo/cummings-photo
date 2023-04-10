2ca1495f4cc8eed367b3df5fc445fa1eaa064fcf
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2023-04-10 16:28:04 -0400

    Update collage-list.js

--- html-code/js/collage-list.js
+++ html-code/js/collage-list.js
@@ -4,7 +4,7 @@ const images = [
    {"url": "../html-collages/03.png", "label": "street market confusion pt.2, 2020"},
    {"url": "../html-collages/04.png", "label": "street market confusion pt.3, 2020"},
    {"url": "../html-collages/05.png", "label": "manhattan is a port city, 2020"},
-   {"url": "../html-collages/06.png", "label": "companion, 2020"},
+   {"url": "../html-collages/06.png", "label": "some bullshit, 2020"},
    {"url": "../html-collages/07.png", "label": "going south, 2020"},
    {"url": "../html-collages/merrr.png", "label": "untitled, 2021"},
    {"url": "../html-collages/icecream.png", "label": "special delivery, 2021"},
