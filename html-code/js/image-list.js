1cc4641b7d12726eab12eda907cca4d5e3e87c4b
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2023-04-10 15:38:18 -0400

    Update image-list.js

--- html-code/js/image-list.js
+++ html-code/js/image-list.js
@@ -4,7 +4,6 @@ const images = [
   {"url": "../html-pics/11.png", "label": "williamsburg, nyc, 2020"},
   {"url": "../html-pics/07.png", "label": "acadia natl park, me, 2020"},
   {"url": "../html-pics/14.png", "label": "lower east side, ny, 2020"},
-  {"url": "../html-pics/08.png", "label": "lower east side, ny, 2020"},
   {"url": "../html-pics/20.png", "label": "lower east side, ny, 2020"},
   {"url": "../html-pics/21.png", "label": "queens, ny, 2020"},
   {"url": "../html-pics/09.png", "label": "soho, nyc, 2019"},
