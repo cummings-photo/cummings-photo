5b07fc035e4a4d1be31233a2353124f32ada154c
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2023-04-10 20:57:35 -0400

    Update image-list.js

--- html-code/js/image-list.js
+++ html-code/js/image-list.js
@@ -1,16 +1,16 @@
 const images = [
-  {"url": "../html-pics/01.png", "label": "lower manhattan, nyc, 2019"},
-  {"url": "../html-pics/10.png", "label": "new hyde park, ny, 2020"},
-  {"url": "../html-pics/11.png", "label": "williamsburg, nyc, 2020"},
-  {"url": "../html-pics/07.png", "label": "acadia natl park, me, 2020"},
-  {"url": "../html-pics/14.png", "label": "lower east side, ny, 2020"},
-  {"url": "../html-pics/20.png", "label": "lower east side, ny, 2020"},
-  {"url": "../html-pics/21.png", "label": "queens, ny, 2020"},
-  {"url": "../html-pics/09.png", "label": "soho, nyc, 2019"},
-  {"url": "../html-pics/04.png", "label": "freeport, ny, 2020"},
-  {"url": "../html-pics/13.png", "label": "new hyde park, ny, 2020"},
-  {"url": "../html-pics/16.png", "label": "freeport, ny, 2020"},
-  {"url": "../html-pics/03.png", "label": "lower east side, ny, 2019"},
+  {"url": "../html-pics/00054870026.jpg", "label": "roslyn, ny, 2020"},
+  {"url": "../html-pics/34381E8F-33F3-4953-BEFB-0FC4742B6209.jpeg", "label": "astoria, nyc, 2021"},
+  {"url": "../html-pics/000055280006.JPG", "label": "flushing, nyc, 2022"},
+  {"url": "../html-pics/000548750012.jpg", "label": "astoria park, nyc, 2021"},
+  {"url": "../html-pics/6BA85703-6D66-46DF-84CB-11AC62BD0217.jpg", "label": "jackson heights, nyc, 2021"},
+  {"url": "../html-pics/000055300006.jpg", "label": "williamsburg, ny, 2022"},
+  {"url": "../html-pics/62401F97-36F5-413B-A5EC-A86BC5AC22BF.jpg", "label": "jackson heights, nyc, 2021"},
+  {"url": "../html-pics/000055280010.jpg", "label": "flushing, nyc, 2022"},
+  {"url": "../html-pics/00055300018.jpg", "label": "williamsburg, nyc, 2022"},
+  {"url": "../html-pics/000055300014.jpg", "label": "williamsburg, ny, 2020"},
+  {"url": "../html-pics/000039980009.tif", "label": "louis gantry state park, nyc, 2022"},
+  {"url": "../html-pics/000039960023.tif", "label": "mccarren park, nyc, 2022"},
 
 
 ]
