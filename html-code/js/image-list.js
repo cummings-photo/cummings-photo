93525e5dc1f0876127e7140b5e4440f7d770f83f
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2023-04-10 21:29:58 -0400

    Update image-list.js

--- html-code/js/image-list.js
+++ html-code/js/image-list.js
@@ -1,5 +1,5 @@
 const images = [
-  {"url": "../html-pics/00054870026.jpg", "label": "roslyn, ny, 2020"},
+  {"url": "../html-pics/000548750026.jpg", "label": "roslyn, ny, 2020"},
   {"url": "../html-pics/34381E8F-33F3-4953-BEFB-0FC4742B6209.jpeg", "label": "astoria, nyc, 2021"},
   {"url": "../html-pics/000055280006.JPG", "label": "flushing, nyc, 2022"},
   {"url": "../html-pics/000548750012.jpg", "label": "astoria park, nyc, 2021"},
@@ -7,7 +7,7 @@ const images = [
   {"url": "../html-pics/000055300006.jpg", "label": "williamsburg, ny, 2022"},
   {"url": "../html-pics/62401F97-36F5-413B-A5EC-A86BC5AC22BF.jpg", "label": "jackson heights, nyc, 2021"},
   {"url": "../html-pics/000055280010.jpg", "label": "flushing, nyc, 2022"},
-  {"url": "../html-pics/00055300018.jpg", "label": "williamsburg, nyc, 2022"},
+  {"url": "../html-pics/000055300018.jpg", "label": "williamsburg, nyc, 2022"},
   {"url": "../html-pics/000055300014.jpg", "label": "williamsburg, ny, 2020"},
   {"url": "../html-pics/000039980009.tif", "label": "louis gantry state park, nyc, 2022"},
   {"url": "../html-pics/000039960023.tif", "label": "mccarren park, nyc, 2022"},
