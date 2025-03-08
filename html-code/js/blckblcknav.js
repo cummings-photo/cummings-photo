5137da880c51ef68b9e2315ebc792c018cbae985
cummings-photo <77297890+cummings-photo@users.noreply.github.com>
2025-03-08 18:51:11 -0500

    Update blckblcknav.js

--- html-code/js/blckblcknav.js
+++ html-code/js/blckblcknav.js
@@ -49,8 +49,8 @@ window.onkeyup = function (e) {
     try { window.location.replace("https://studio-cjc.com/html-code/blckcrtns.html"); } 
     catch(e) { window.location = "https://studio-cjc.com/html-code/blckcrtns.html"; };
   } else if (key === 40) {
-   try { window.location.replace("https://studio-cjc.com/html-code/ties.html"); } 
-   catch(e) { window.location = "https://studio-cjc.com/html-code/ties.html"; };
+    try { window.location.replace("https://studio-cjc.com/html-code/easteregg.html"); } 
+    catch(e) { window.location = "https://studio-cjc.com/html-code/easteregg.html"; };
   } 
 }
 
